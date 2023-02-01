dbdebf85afec42b186fe7594357b512ad547e190
Raphael <okolonnaemeka2017@gmail.com>
2023-02-01 14:38:00 -0500

    Fix linters error

--- src/modules/add-score.js
+++ src/modules/add-score.js
@@ -1,11 +1,11 @@
-const addApiScore =  async(user) => {
-    const response = fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/np8G7JdKLR96QcXBTm5K/scores/', {
+const addApiScore = async (user) => {
+  const response = fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/np8G7JdKLR96QcXBTm5K/scores/', {
     method: 'POST',
     body: JSON.stringify(user),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  })
+  });
   const scoreDataPosted = await response.json();
   return scoreDataPosted;
 };
