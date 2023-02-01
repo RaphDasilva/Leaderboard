a8c66d78c0b638ccfc3e0c27cda55ea518f6b335
Raphael <okolonnaemeka2017@gmail.com>
2023-02-01 11:26:17 -0500

    Update getscore

--- src/modules/get-score.js
+++ src/modules/get-score.js
@@ -1,7 +1,8 @@
 const getScore = async () => {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/np8G7JdKLR96QcXBTm5K/scores/');
-    const result = await response.json();
-    const scoreList = await result.result;
+    const results = await response.json();
+    const scoreList = results.result;
     return scoreList;
-}
-export default getScore;
+  };
+  
+  export default getScore;
